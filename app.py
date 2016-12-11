4ef18d599534ae2485485d1b5ea7ddfb08b3d546
Sean Smith <swsmith@bu.edu>
2016-12-11 19:30:09 +0000

    A is for Apple. 		-- Hester Pryne

--- app.py
+++ app.py
@@ -14,18 +14,19 @@ def get_borough():
 	lng = float(request.form['long'])
 	print "lat = %f" % (lat)
 	print "long = %f" % (lng)
-	return borough(lat, lng)
+	return borough(lat, lng, 'static/nyc_boroughs.json')["BoroName"]
 
 
-def borough(lat, lng):
-	with open('nyc_boroughs.json') as data_file:
+def borough(lat, lng, path):
+	with open(path) as data_file:
 		data = json.load(data_file)
 		geo = geojson.FeatureCollection(data["features"])
 		for feature in geo['features']:
 			multipolygon = feature["geometry"]["coordinates"]
-			if in_multipolygon(lat, lng, multipolygon):
-				return feature["properties"]["BoroName"] + "<br>"
-	return "Not in a NYC Borough"
+			is_multipolygon = feature["geometry"]["type"] == "MultiPolygon"
+			if in_polygon(lat, lng, multipolygon, is_multipolygon):
+				return feature["properties"]
+	return "Not found."
 
 def x(lnglat):
 	return lnglat[0]
@@ -33,9 +34,10 @@ def x(lnglat):
 def y(lnglat):
 	return lnglat[1]
 
-def in_multipolygon(lat, lng, multipolygon):
-	for coord in multipolygon:
-		coord = coord[0]
+def in_polygon(lat, lng, polygon, is_multipolygon):
+	for coord in polygon:
+		if is_multipolygon:
+			coord = coord[0]
 		c = False
 		j = len(coord) - 1;
 		for i in range(len(coord)):
@@ -50,20 +52,26 @@ def in_multipolygon(lat, lng, multipolygon):
 	return False
 
 
-# lat, long 
-# 40.707438, -74.006302
-# Assuming lat = Y and long = X
-print "Staten Island (%f, %f)" % (40.6306300839918, -74.08407211303711)
-print(borough(40.6306300839918, -74.08407211303711))
+def test():
+	# lat, long 
+	# 40.707438, -74.006302
+	# Assuming lat = Y and long = X
+	print "Staten Island (%f, %f)" % (40.6306300839918, -74.08407211303711)
+	print(borough(40.6306300839918, -74.08407211303711, 'static/nyc_boroughs.json')["BoroName"])
 
-print "Manhattan (%f, %f)" % (40.7189000170401, -73.99171829223633)
-print(borough(40.7189000170401, -73.99171829223633))
+	print "Manhattan (%f, %f)" % (40.7189000170401, -73.99171829223633)
+	print(borough(40.7189000170401, -73.99171829223633, 'static/nyc_boroughs.json')["BoroName"])
 
-print "Bronx (%f, %f)" % (40.822383381646446, -73.91275405883789)
-print(borough(40.822383381646446, -73.91275405883789))
+	print "Bronx (%f, %f)" % (40.822383381646446, -73.91275405883789)
+	print(borough(40.822383381646446, -73.91275405883789, 'static/nyc_boroughs.json')["BoroName"])
 
-print "Brooklyn (%f, %f)" % (40.68948969132278, -73.98347854614258)
-print(borough(40.68948969132278, -73.98347854614258))
+	print "Brooklyn (%f, %f)" % (40.68948969132278, -73.98347854614258)
+	print(borough(40.68948969132278, -73.98347854614258, 'static/nyc_boroughs.json')["BoroName"])
 
-print "Queens (%f, %f)" % (40.748037258302986, -73.88279914855957)
-print(borough(40.748037258302986, -73.88279914855957))
+	print "Queens (%f, %f)" % (40.748037258302986, -73.88279914855957)
+	print(borough(40.748037258302986, -73.88279914855957, 'static/nyc_boroughs.json')["BoroName"])
+
+	print "Fenway/Kenmore (%f, %f)" % (42.348688, -71.102873)
+	print(borough(42.348688, -71.102873, 'static/boston.geojson')["name"])
+
+# test()
--- static/maps.js
+++ static/maps.js
@@ -4,6 +4,29 @@ var marker = null;
 var map = null;
 var infowindow = null;
 
+var boston = {
+	name: "Boston",
+	loc: {lat: 40.7189000170401, lng: -73.99171829223633},
+	zoom: 12,
+	geojson: 'static/boston.geojson'
+};
+
+var nyc = {
+	name: "NYC",
+	loc: {lat: 42.348688, lng: -71.102873},
+	zoom: 12,
+	geojson: 'static/nyc_boroughs.json'
+};
+
+$(document).ready(function() {
+	$("#data").change(function(data) {
+		console.log($(this).val());
+		map.data.setStyle({});
+		load_geojson($(this).val());
+	});
+});
+
+
 function openWindow(data) {
 	infowindow = new google.maps.InfoWindow({
 		content: data
@@ -24,9 +47,10 @@ function addMarker(location, map) {
 }
 
 function initMap() {
+	var location = boston;
 	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 12,
-		center: {lat: 40.707438, lng: -74.006302}
+		zoom: location.zoom,
+		center: location.loc
 	});
 	google.maps.event.addListener(map, 'click', function(event) {
 		if (marker) {
@@ -35,17 +59,34 @@ function initMap() {
 		}
 		addMarker(event.latLng, map);
 	});
-	load_geojson()
+	load_geojson(location.geojson);
 }
 
-function load_geojson() {
-	map.data.loadGeoJson('static/nyc_boroughs.json');
+function load_geojson(path) {
+	map.data.loadGeoJson(path);
 	map.data.setStyle(function(feature) {
-		var num = feature.getProperty('BoroCode') - 1;
-		var colors = ['red', 'green', 'blue', 'yellow', 'purple'];
-		return {
-			fillColor: colors[num],
-			strokeWeight: 1
-		};
+		return get_nyc_style(feature);
 	});
+}
+
+function get_nyc_style(feature) {
+	var num = feature.getProperty('BoroCode') - 1;
+	var colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+	return {
+		fillColor: colors[num],
+		title: feature.getProperty('BoroName'),
+		strokeWeight: 1,
+		fillOpacity: .1,
+		clickable: false
+	};
+}
+
+function get_boston_style(feature) {
+	return {
+		fillColor: feature.getProperty('fill'),
+		title: feature.getProperty('name'),
+		strokeWeight: 1,
+		fillOpacity: feature.getProperty('fill-opacity'),
+		clickable: false
+	};
 }
